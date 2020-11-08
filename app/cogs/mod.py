93ef6ba55b80b0458a9bf86e8814e7345f681c6a
Lucas <circuitsacul@gmail.com>
2020-11-08 15:00:01 -0500

    working ban, unban, and kick

--- app/cogs/mod.py
+++ app/cogs/mod.py
@@ -1,4 +1,6 @@
 import discord
+from discord import utils
+from typing import Union
 from discord.ext import commands
 
 
@@ -38,6 +40,122 @@ class Mod(commands.Cog):
             message += f"\n** - {user}:** {num}"
 
         await ctx.send(message, delete_after=10)
+
+    @commands.command(name="ban")
+    @commands.has_guild_permissions(ban_members=True)
+    async def ban(self, ctx, user: Union[discord.Member, int], *, reason=None):
+        if not isinstance(user, int):
+            if ctx.author.top_role.position <= user.top_role.position \
+                    and ctx.guild.owner_id != ctx.author.id:
+                await ctx.send(
+                    "You cannot ban this user because their role "
+                    "is higher than or equal to yours."
+                )
+                return
+        if isinstance(user, int):
+            user_str = f"<@{user}>"
+            user = discord.Object(id=user)
+        else:
+            user_str = user
+        try:
+            await user.send(
+                f"You have been **banned** from **{ctx.guild}** server "
+                f"due to the following reason:\n**{reason}**"
+            )
+        except Exception:
+            pass
+        await ctx.guild.ban(user, reason=reason)
+        if reason:
+            await ctx.send(
+                f"User **{user_str}** has been banned for reason: "
+                f"**{reason}**."
+            )
+        else:
+            await ctx.send(f"User **{user_str}** has been banned.")
+
+    @commands.command(name="unban")
+    @commands.has_guild_permissions(ban_members=True)
+    @commands.guild_only()
+    async def unban(
+        self, ctx, user: Union[discord.User, int, str],
+        *, reason=None
+    ):
+        if isinstance(user, int):
+            user_str = f"<@{user}>"
+            user = discord.Object(id=user)
+        else:
+            user_str = user
+
+        if isinstance(user, str):
+            guild_bans = await ctx.guild.bans()
+            print(guild_bans)
+            try:
+                name, tag = user.split('#')
+                print(f"<{name}>")
+                print(f"<{tag}>")
+            except Exception:
+                await ctx.send(
+                    "Please format the username like this: "
+                    "Username#0000"
+                )
+                return
+
+            banned_user = None
+            for gb in guild_bans:
+                if gb.user.name == name:
+                    if gb.user.discriminator == tag:
+                        banned_user = gb
+
+            if banned_user is None:
+                await ctx.send("I could not find that user in the bans.")
+                return
+            await ctx.guild.unban(banned_user.user)
+            try:
+                await banned_user.send(
+                    f"You have been unbanned with reason: {reason}"
+                )
+            except Exception:
+                pass
+
+        else:
+            await ctx.guild.unban(user)
+            try:
+                await user.send(
+                    f"You have been unbanned with reason: {reason}"
+                )
+            except Exception:
+                pass
+
+        await ctx.send(f"Unbanned **{user_str}**")
+
+    @commands.command(name="kick")
+    @commands.has_guild_permissions(kick_members=True)
+    async def kick(self, ctx, user: discord.Member = None, *, reason=None):
+        if reason is None:
+            reason = "No reason provided."
+
+        if ctx.author.top_role.position <= user.top_role.position \
+                and ctx.guild.owner_id != ctx.author.id:
+            await ctx.send(
+                "You cannot kick this user because their role is "
+                "higher than or equal to yours."
+            )
+        else:
+            await ctx.guild.kick(user, reason=reason)
+            try:
+                await user.send(
+                    f"You have been **kicked** from **{ctx.guild}** "
+                    f"server due to the following reason:\n**{reason}**"
+                )
+            except Exception:
+                pass
+            if reason:
+                await ctx.send(
+                    f"User **{user}** has been kicked "
+                    "for reason: **{reason}**."
+                )
+            else:
+                await ctx.send(f"User **{user}** has been kicked.")
 
 
 def setup(bot):
